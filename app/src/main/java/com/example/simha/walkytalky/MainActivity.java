890f34fdd66a7a5fb82c679581c59f0e403d68cf
shashank-simha <shashank-simha@github.com>
2018-09-05 22:42:19 +0530

    QR scan added

--- app/src/main/java/com/example/simha/walkytalky/MainActivity.java
+++ app/src/main/java/com/example/simha/walkytalky/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.simha.walkytalky;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//implementing onclicklistener
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //View Objects
+    private Button buttonScan;
+
+    //qr code scanner object
+    private IntentIntegrator qrScan;
+
+    private ArrayAdapter ClueAdapter;
+
+    private List<String> cluesList = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //View objects
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
+
+        //attaching onclick listener
+        buttonScan.setOnClickListener(this);
+
+        ListView clueView = (ListView) findViewById(R.id.clues);
+        ClueAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,cluesList);
+        clueView.setAdapter(ClueAdapter);
+    }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            //if qrcode has nothing in it
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                MainActivity.this.cluesList.add(result.getContents());
+                MainActivity.this.ClueAdapter.notifyDataSetChanged();
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        //initiating the qr code scan
+        qrScan.initiateScan();
     }
 }
